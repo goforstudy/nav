acf18d49663bc7c241904b2edddaacdb4db7a4b2
wangtao <1182792703@qq.com>
2021-05-30 21:56:11 +0800

    完成mobile版

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 一个前端导航网站
+这是设计稿[链接](https://www.figma.com/file/Umodz2Uj61H1N2pbCse48o/%E5%89%8D%E8%88%AA?node-id=0%3A1)
---
+++ src/main.js
@@ -0,0 +1,119 @@
+const links = JSON.parse(localStorage.getItem("links"));
+const hasMap = links ? links : [
+    {logo: 'A', logoType: 'text', url: "https://www.acfun.cn"},
+    {logo: 'B', logoType: 'text', url: "https://bilibili.com"},
+]
+
+const $siteList = $(".siteList");
+const $last = $siteList.find("li.last")
+function simplifyUrl (url) {
+    return url.replace("http://", "")
+            .replace("https://","")
+            .replace("www.", "")
+            .replace(/\/.*/,  '')
+}
+// function removeLink (event,index) {
+//     hasMap.splice(index, 1);
+//     event.stopPropagation();
+//     console.log(event, index,)
+// }
+const render = () => {
+    
+    $siteList.find("li:not(.last)").remove();
+    hasMap.forEach( (node, index) =>{ 
+        // const $li = $(
+        // `
+        // <li>
+        //     <a href="${node.url}">
+        //         <div class="site">
+        //             <div class="remove">
+        //                 <svg class="icon" aria-hidden="true">
+        //                     <use xlink:href="#icon-delete"></use>
+        //                 </svg>
+        //             </div>
+        //             <div class="logo">
+        //                 ${node.logo}
+        //             </div>
+        //             <div class="link">
+        //                 ${simplifyUrl(node.url)}
+        //             </div>
+        //         </div>
+        //     </a>
+    
+        // </li>
+        // `
+        // ).insertBefore($last);
+        const $li = $(
+        `
+        <li>
+                <div class="site">
+                    <div class="remove">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-delete"></use>
+                        </svg>
+                    </div>
+                    <div class="logo">
+                        ${node.logo}
+                    </div>
+                    <div class="link">
+                        ${simplifyUrl(node.url)}
+                    </div>
+                </div>
+    
+        </li>
+        `
+        ).insertBefore($last);
+        $li.on(
+            'click', ()=>{
+                window.open(node.url);
+            }
+        );
+        $li.on(
+            'click',
+            '.remove',
+            (e) => {
+                hasMap.splice(index, 1);
+                render();
+                e.stopPropagation()
+            }
+        )
+    });
+
+}
+
+
+render()
+$(".addButton")
+.on("click", () => {
+    let url = window.prompt("请输入新增网址：");
+    if(url.indexOf('http') !== 0 || url.indexOf('https') !== 0){
+        url = "https://" + url;
+    }
+    hasMap.push({
+        logo: simplifyUrl(url)[0].toUpperCase(),
+        logoType: 'text',
+        url: url
+    })
+    render();
+    // const $site = $(
+    //     `
+    //     <li>
+    //         <a href="${url}">
+    //             <div class="site">
+    //                 <div class="logo">
+    //                     ${url.split("//")[1][0].toLocaleUpperCase()}
+    //                 </div>
+    //                 <div class="link">${url}</div>
+    //             </div>
+    //         </a>
+
+    //     </li>
+    //     `
+    // ).insertBefore($last)
+})
+
+window.onbeforeunload = () => {
+    const str = JSON.stringify(hasMap);
+    window.localStorage.setItem("links", str)
+}
+
